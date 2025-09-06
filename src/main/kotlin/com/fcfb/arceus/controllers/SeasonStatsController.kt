6ed4ed8b5d77a15926ed3f5ea42049311f9ee87b
Andrew Kicklighter <apkicklighter@gmail.com>
2025-09-05 19:35:16 -0500

    fix build issues

--- src/main/kotlin/com/fcfb/arceus/controllers/SeasonStatsController.kt
+++ src/main/kotlin/com/fcfb/arceus/controllers/SeasonStatsController.kt
@@ -5,6 +5,7 @@ import com.fcfb.arceus.service.fcfb.SeasonStatsService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
