084952859ef37742339cc2fbe27b2081d629c6dc
Andrew Kicklighter <apkicklighter@gmail.com>
2026-02-10 20:22:38 -0600

    run linting

--- src/main/kotlin/com/fcfb/arceus/repositories/ConferenceRulesRepository.kt
+++ src/main/kotlin/com/fcfb/arceus/repositories/ConferenceRulesRepository.kt
@@ -2,7 +2,6 @@ package com.fcfb.arceus.repositories
 
 import com.fcfb.arceus.enums.team.Conference
 import com.fcfb.arceus.model.ConferenceRules
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
